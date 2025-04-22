478e077cd8a0202d05b31ec18f9d117641f8098d
cesar <cesar.a.neri@gmail.com>
2025-04-22 17:06:29 -0600

    additional design clean up before adding new pages

--- js/carousel.js
+++ js/carousel.js
@@ -2,9 +2,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const introImage = document.querySelector('#introImage img');
     const images = [
       'assets/carousel/espiropapas.png',
-      'assets/carousel/vaca.png',
-      'assets/carousel/frutas.png',
+      'assets/carousel/truck.png',
       'assets/carousel/flores.png',
+      'assets/carousel/caballos.png',
       // Add more image paths here
     ];
     let currentIndex = 0;
